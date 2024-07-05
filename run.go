b276e8cc65a4a1094366c942766b8c576b4cd9d4
mbukeRepo <mbuke404@gmail.com>
2024-07-05 10:23:04 +0200

    fix: clean up

--- run.go
+++ run.go
@@ -5,6 +5,7 @@ import "context"
 type InferenceOptions struct{}
 type InferenceResults struct{}
 
+// TODO: implement single request runs on fal.
 func (c *Client) Run(ctx context.Context, client *Client, options *InferenceOptions) (*InferenceResults, error) {
 	return nil, nil
 }
